e477fc1c21c3a5086cce4ffde08a06d73b419e5f
Pedro Morales <pedrojmorales01@gmail.com>
2022-06-14 16:10:18 -0700

    added docstrings to button.py

--- game/button.py
+++ game/button.py
@@ -1,7 +1,22 @@
+# This file defines the Button class used to create interactable buttons.
+
+"""This file contains the Button class used to create buttons."""
 import pygame
 
 class Button:
-    def __init__(self, center, screen, value, text, width=100, height=50, color=(255, 0, 0), border_on=True):
+    """Class used to create buttons."""
+    def __init__(
+    self,
+    center,
+    screen,
+    value,
+    text,
+    width=100,
+    height=50,
+    color=(255, 0, 0),
+    border_on=True
+    ):
+        """Initialize a button."""
         self._center = center
         self._screen = screen
         self._width = width
@@ -19,42 +34,52 @@ class Button:
 
     @property
     def rect(self):
+        """Return bounding rect."""
         left = self._center[0] - self._width
         top = self._center[1] - self._height
         return pygame.Rect(left, top, self._width, self._height)
 
     @property
     def value(self):
+        """Return the value of the Button."""
         return self._value
 
     @property
     def pressed(self):
+        """Return true if the button has been pressed."""
         return self._pressed
 
     def unpress(self):
+        """Set the button to become unpressed."""
         self._pressed = False
 
     def draw_border(self, surface):
+        """Draw the border surrounding the button."""
         pygame.draw.rect(surface, (0, 0, 0), self.rect, self._width // 50, 10)
 
     def draw(self, surface):
+        """Draw the button."""
         pygame.draw.rect(surface, self._color, self.rect, self._width, 10)
         surface.blit(self._text, self._textpos)
         if self._border_on:
             self.draw_border(surface)
 
     def change_color(self, color):
+        """Change the color of the button."""
         self._color = color
 
     def process_events(self, event):
+        """Process game events to determine if button is being clicked."""
         point = pygame.mouse.get_pos()
 
         if event.type == pygame.MOUSEMOTION:
+            # Change the color of the button if the mouse is hovering over it
             if self.rect.collidepoint(point[0], point[1]):
                 self.change_color((106, 238, 39))
             else:
                 self.change_color(self._original_color)
 
+        # Represent the button being clicked
         if self.rect.collidepoint(point) and event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed():
                 self._pressed = True
