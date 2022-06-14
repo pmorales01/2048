04a4547c5d8ea89c330368da8d7a87c60e05c292
Pedro Morales <pedrojmorales01@gmail.com>
2022-06-14 16:12:05 -0700

    added docstrings to colors.py

--- game/colors.py
+++ game/colors.py
@@ -1,4 +1,9 @@
-# color = (tile_color, text_color)
+# This file defines various pairs of RGB colors to use for the background and
+# text of Tile objects.
+
+"""This file creates a list of RBG pairs of colors to use for Tile objects."""
+
+# Each pair represents COLOR = (TILE_COLOR, TEXT_COLOR)
 WHITE = (255, 255, 255)
 DARK = (129, 120, 110)
 LIGHT_YELLOW = ((238, 225, 201), DARK)
@@ -11,5 +16,7 @@ BLUE = ((100, 197, 249), WHITE)
 LIGHT_BLUE = ((99, 197, 243), WHITE)
 NAVY = ((30, 60, 127), WHITE)
 RED = ((255, 0, 0), WHITE)
+
+# List of colors to be used
 COLORS = [LIGHT_YELLOW, LIGHTER_ORANGE, LIGHT_ORANGE, LIGHT_RED_ORANGE, ORANGE,
 DARK_YELLOW, BLUE, LIGHT_BLUE, NAVY, RED]
