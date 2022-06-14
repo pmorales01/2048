7b287ba64b152eae2cb2da567aa4bf4de71ff2e6
Pedro Morales <pedrojmorales01@gmail.com>
2022-06-14 16:22:46 -0700

    added docstrings to game.py

--- game/game.py
+++ game/game.py
@@ -1,17 +1,20 @@
+# This file contains the Game class which is used to run and create 2048.
+
+"""The Game class creates an instance of 2048 and manages scenes."""
+
 import pygame, sys, os
 from pygame.locals import *
-
 from game.scene import (
     TitleScene,
     VideoGameScene
 )
 
 class Game:
-    def __init__(self,
-    height=800,
-    width=800,
-    window_title="2048"):
-
+    """Game class used to create an instance of 2048."""
+    def __init__(
+        self, height=800, width=800, window_title="2048"
+    ):
+        """Initialize the game."""
         pygame.init()
         self._window_size = (width, height)
         self._clock = pygame.time.Clock()
@@ -20,10 +23,12 @@ class Game:
         self._scenes = []
 
     def create_scenes(self):
+        """Create the scenes the game will use."""
         self._scenes = [TitleScene(self._screen, (64, 128, 237)),
             VideoGameScene(self._screen)]
 
     def run(self):
+        """Start and run each scene."""
         for scene in self._scenes:
             scene.start_scene()
             while scene.scene_is_running:
